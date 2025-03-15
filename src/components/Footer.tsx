ca67aa461495aedb00b323daa96d223acc64f807
camw0 <cmrxn2005@gmail.com>
2025-03-15 22:33:19 +0000

    Update logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ export default () => (
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
             <div class="md:flex md:justify-between">
                 <div class="mb-6 md:mb-0">
-                    <a href="https://flowbite.com/" class="flex items-center">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
+                    <a href="/" class="flex items-center">
+                        <img src="https://i.postimg.cc/zX0BVThh/Untitled-design-2.png" class="h-8 me-3" />
                         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CXMPUTE</span>
                     </a>
                 </div>
